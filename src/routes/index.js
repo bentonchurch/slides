6c0baa1cabd69c23b274a8b2e1e811e263bf87d1
Eli <136011323+EliasElliotson@users.noreply.github.com>
2025-03-23 01:31:08 +0000

    Added a link to the homepage to get to the dummy import

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import { Page } from "../components/Page.js";
 
 function HomePage() { 
   return (
     <Page>
       <h1>Home</h1>
+      <a href="import/">Import page</a>
     </Page>
   );
 }
